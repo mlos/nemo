b9125b5a2f3596c88f2973e834d2836dbfee8bd1
mlos <mlos@gmx.com>
2018-04-16 19:16:32 +0000

    new

---
+++ dist/inbus-server.py
@@ -0,0 +1,7 @@
+#
+# (c) 2018 Maarten Los. All rights reserved.
+# 
+from inbus.server.inbus import Inbus
+
+# Runs Inbus message bus
+Inbus().run()
--- dist/power.py
+++ dist/power.py
@@ -1,16 +1,24 @@
 #
-# (c) 2017 Maarten Los. All rights reserved.
+# (c) 2017, 2018 Maarten Los. All rights reserved.
 # 
 #
 import RPi.GPIO as GPIO
 import time
+import json
 from subprocess import call
+from inbus.client.publisher import Publisher
 
 Debug=0
 
 PWR_BTN_IN = 18
 LED_R_CTL = 23
 LED_G_CTL = 24
+
+AppKey = "nemo"
+MaxAppType = 2
+AppTypeCopyright = 0
+AppTypePower = 1
+currAppType = 0
 
 PowerButtonPressedDuration = 3 # seconds
 
@@ -33,6 +41,8 @@ GPIO.output(LED_R_CTL, True)
 
 call(["touch","/tmp/power"])
 
+inbus = Publisher(AppKey)
+
 # Start polling
 while True:
   print "."
@@ -45,12 +55,18 @@ while True:
       # Reset pin to input and pull-down. This will make sure LED_R will
       # light up by default
       GPIO.setup(LED_R_CTL, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+      inbus.publish("", AppTypePower)
       if Debug:
         print "Powering off (not for real)"
       else:
         call(["poweroff"])
       exit(0)
-  print "NO Power off"
+
+  inbus.publish(
+        json.dumps({'ip': '1,2,3,4',
+                    'version': '1.0'}),
+        AppTypeCopyright)
+
   call(["touch","/tmp/NO.power.off"])
   
 
